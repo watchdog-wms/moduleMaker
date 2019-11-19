a96cf647360ac7ea25b8c20cf8f3213848a5eb46
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2019-11-19 14:56:27 +0100

    fixed error in starter script

--- distribute/moduleMaker.sh
+++ distribute/moduleMaker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 SCRIPT_FOLDER=$(cd $(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null || readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")) && pwd -P)
+cd "${SCRIPT_FOLDER}/../moduleMaker/"
 source "${SCRIPT_FOLDER}/../core_lib/javafxLauncher.sh" "../moduleMaker/moduleMaker.jar" $@
